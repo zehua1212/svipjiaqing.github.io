b960b0158feea7c0a83ac4838c5c94185beae390
svipjiaqing <46028914+svipjiaqing@users.noreply.github.com>
2019-10-04 09:42:43 +0800

    Add files via upload

---
+++ spot.js
@@ -0,0 +1,56 @@
+﻿(function() {
+    'use strict';
+
+    var canvas = document.getElementById('canvas');
+
+    var engine = new Shape.Engine(canvas);
+    var promise = new Promise((resolve) => { resolve(); });
+    
+    document.getElementById('go').addEventListener('click', start);
+    
+    function start() {
+        document.getElementById('go').removeEventListener('click', start);
+        promise.then(() =>engine.toText('泽华 '))
+
+        .then(() => engine.shake())
+
+         .then(() => engine.toText('YOU '))
+
+        .then(() => engine.shake())
+
+        .then(() => engine.toText('ARE '))
+
+        .then(() => engine.shake())
+
+        .then(() => engine.toText('MY'))
+
+        .then(() => engine.shake())
+
+        .then(() => engine.toText('Angel '))
+
+        .then(() => engine.shake())
+
+        .then(() => engine.toText('I'))
+
+        .then(() => engine.shake())
+
+        .then(() => engine.toText('DONT'))
+
+        .then(() => engine.shake())
+
+        .then(() => engine.toText('MISS'))
+
+        .then(() => engine.shake())
+
+        .then(() => engine.toText('YOU'))
+
+        .then(() => engine.shake())
+
+        .then(() => engine.toText('2019.10.05'))
+
+        .then(() => engine.shake())
+
+        .then(() => engine.clear())
+        .then(() => document.getElementById('go').addEventListener('click', start));
+    }
+})();
